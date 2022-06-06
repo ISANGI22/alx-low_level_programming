833a28e5d21de93e0dbeee017baaae585c545d29
KAMANDA <kamandamodeste@gmail.com>
2022-06-06 16:56:54 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * This is the main function
+ *
+ *
+ * Return: return 1 (Success)
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+
+	return (1);
+}
