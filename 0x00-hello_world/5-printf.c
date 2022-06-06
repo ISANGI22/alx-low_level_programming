356cb58ce2d59d6dfba7804cac9c6d77ac031137
KAMANDA <kamandamodeste@gmail.com>
2022-06-06 15:47:09 -0700

    ptinff.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * This is the main function
+ *
+ *
+ * Return: always return 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
