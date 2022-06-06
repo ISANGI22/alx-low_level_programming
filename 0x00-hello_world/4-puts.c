ff4c2230d8de26b1b5c308fbf0d9c184da1b1b1c
KAMANDA <kamandamodeste@gmail.com>
2022-06-06 15:42:13 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * This is the main function
+ *
+ *
+ * Return: always return 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming like building a multilingual puzzle");
+
+	return (0);
+}
