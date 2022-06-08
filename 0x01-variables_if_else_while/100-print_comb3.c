8fa86814da15fbcd340707fd38b0e0340d3abe96
KAMANDA <kamandamodeste@gmail.com>
2022-06-08 09:21:44 -0700

    100 print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = n + 1; m <=9; m++)
+		{
+			putchar((n) + '0');
+			putchar((m) + '0');
+
+			if (n == 8 && m == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+}
