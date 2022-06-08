33a38703da9d8b2507fec800fc01c44475ab0f59
KAMANDA <kamandamodeste@gmail.com>
2022-06-07 20:54:30 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	putchar('\n');
+
+	return (0);
+}
