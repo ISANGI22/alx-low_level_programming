3bafc377c5544723ef13e28037f7b85402fe4f3f
KAMANDA <kamandamodeste@gmail.com>
2022-06-09 06:11:48 -0700

    0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print _putchar with new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
