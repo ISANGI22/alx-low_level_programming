bdd84b690e86606e23a1b44ac3c04b73f9093f80
KAMANDA <kamandamodeste@gmail.com>
2022-06-10 10:56:53 -0700

    5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,6 @@
  * @n: The caracter passed over for checking
  *
  * Return: return 1 and prints + is n is greter than zero
- * Return 0 and prints 0 if n is zero
- * Return -1 and prints - if n is less than zero 
  */
 int print_sign(int n)
 {
