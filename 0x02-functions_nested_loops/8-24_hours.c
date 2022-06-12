3960752ff797156a0f5c6b54d5227c0ae2840ec4
KAMANDA <kamandamodeste@gmail.com>
2022-06-11 17:18:40 -0700

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,34 +2,24 @@
 
 /**
  * jack_bauer - Print every minute of the day of Jack Bauer
- * @n: integer to be used
- *
- * Return: return value of the integer
+ *		from 00:00 to 23:59 
  */
 void jack_bauer(void)
 {
 	int m;
-	int n;
-	int o;
 	int p;
 
-	for (m = 0; m <=2 ; m++)
+	for (m = 0; m <=23 ; m++)
 	{
-		for (n = 0; n <= 3; n++)
-		{
-			for (o = 0; o <= 5; o++)
+		for (p = 0; p <= 59; p++)
 			{
-				for (p = 0; p <= 9; p++)
-				{
-					_putchar((m) + '0');
-					_putchar((n) + '0');
-					_putchar(':');
-					_putchar((o) + '0');
-					_putchar((p) + '0');
-					_putchar('\n');
-				}
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+				_putchar(':');
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+				_putchar('\n');
 			}
-		}
 	}
 	_putchar('\n');
 }
