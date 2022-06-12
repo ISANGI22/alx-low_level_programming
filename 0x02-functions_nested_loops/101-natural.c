f4a988e0863ef8e021cbc1cf39ed37c1d3ba9b5c
KAMANDA <kamandamodeste@gmail.com>
2022-06-12 02:19:27 -0700

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,16 +5,16 @@
  * main - entry point of the application
  *
  * Return: Always return 0.
- * */
+ */
 int main(void)
 {
 	int n;
-	int m;
+	int m = 0;
 
 	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3) == 0 || (n % 5) == 0)
-			m = m + 1;
+			m = m + n;
 	}
 	printf("%d\n", m);
 
