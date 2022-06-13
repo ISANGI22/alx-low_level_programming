6247a69e5a9ae5a60adcb3994e2ce23d17a920f3
KAMANDA <kamandamodeste@gmail.com>
2022-06-13 02:47:51 -0700

    Adding

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,16 @@
 #include "main.h"
 
 /**
- * positive_or_negative: checking if the number os negative or positive
- *
- * Return: Always 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
-void positive_or_negative(int i)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
 	return (0);
 }
-
