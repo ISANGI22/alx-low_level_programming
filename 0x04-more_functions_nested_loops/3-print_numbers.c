23125c522e8e1db8da25e1656de455ae724e56ba
KAMANDA <kamandamodeste@gmail.com>
2022-06-14 12:05:18 -0700

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * print_numbers - print number from 0 to 9
- *
+ *@a: integer used for loop
+
  * Return: the value of mul
  */
 void print_numbers(void)
 {
 	int a;
-	for (a = 0; a < 10; a++)
+	for (a = '0'; a < '10'; a++)
 		_putchar(a);
 	_putchar('\n');
 }
