3539a948b7764e27be344cb7234c8633f36a7abf
KAMANDA <kamandamodeste@gmail.com>
2022-06-14 16:50:56 -0700

    - ptint line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in terminal
+ *
+ * Return: Always 0
+ */
+void print_line(int n)
+
+{
+	while (n > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
