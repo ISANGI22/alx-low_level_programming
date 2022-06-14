b98523ce2e0d5e957c8680d46bf9e2a961699de3
KAMANDA <kamandamodeste@gmail.com>
2022-06-14 16:14:18 -0700

    5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,10 @@ void more_numbers(void);
 	x = 0;
 	for (x = 0; x <= 10; x++)
 	{
-		for (x = 0; x <= 14; x++)
+		for (y = 0; y <= 14; y++)
 		{
-			_putchar(x + '0');
+			_putchar(y + '0');
 		}
-		_putchar(x + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
