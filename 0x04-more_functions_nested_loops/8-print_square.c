b3106e3b34403e971447d52fc6b97dd6151f78b4
KAMANDA <kamandamodeste@gmail.com>
2022-06-15 11:44:29 -0700

    8 print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a squre
+ * @x: parameter used in the function
+ * @y: parameter used in the function
+ */
+void print_square(int size)
+
+{
+	int x, y;
+
+	if (ize <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
