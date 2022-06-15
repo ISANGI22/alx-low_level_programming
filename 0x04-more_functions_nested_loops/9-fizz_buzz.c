cc4516539d993c407d04db4d6600aa444eb19888
KAMANDA <kamandamodeste@gmail.com>
2022-06-15 12:43:49 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the FizzBuzz
+ * 
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int x;
+	
+	for (i = 1, i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz\n");
+		else if ((i % 5) == 0)
+			printf("Buzz\n");
+		else if ((i % 15) == 0)
+			printf("FIzzBuzz\n");
+		else
+			printf("%d ", x);
+	}
+	return (0);
+}
