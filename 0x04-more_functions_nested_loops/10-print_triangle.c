e858e900e8c4cca974a42b381eda0f0dc6f4c378
KAMANDA <kamandamodeste@gmail.com>
2022-06-16 08:23:43 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,13 +12,13 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (x = 0; x <= size; x++)
+		for (x = 1; x <= size; x++)
 		{
 			for ((y = size - x); y > 0; y--)
 				_putchar(' ');
-			for (x = 0, x < y; y++)
+			for (y = 0, y < x; y++)
 				_putchar('#');
-			if (x ==y)
+			if (x == size)
 				continue;
 			_putchar('\n');
 		}
