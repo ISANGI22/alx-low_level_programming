dfd7a59cf8485a9cef3f7d51f90601ade1f64f30
KAMANDA <kamandamodeste@gmail.com>
2022-09-17 20:14:19 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_number - prints a square
+ * print_number - prints an integer
  * @n: parameter used in the function
  * Return: n
  */
 void print_number(int n)
 
 {
-	unsigned int x = y;
+	int n;
 
-	if (y < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		n = -n;
 	}
-	if (x > 9)
-	{
-		print_number(x / 10);
-	}
-	_putchar(x % 10 + '0');
+
+	if (n/10)
+		_putchar((n / 10) - '0');
+
+	_putchar(n % 10 + '0');
 }
