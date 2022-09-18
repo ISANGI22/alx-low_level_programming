f295911b52a4daba048c35c48925d4e4e3df4e55
KAMANDA <kamandamodeste@gmail.com>
2022-09-17 20:22:24 -0700

    Task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,22 @@
 void print_number(int n)
 
 {
-	int n;
+	int x;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
+	else
+	{
+		x = n;
+	}
 
-	if (n/10)
-		_putchar((n / 10) - '0');
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
 
-	_putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }
