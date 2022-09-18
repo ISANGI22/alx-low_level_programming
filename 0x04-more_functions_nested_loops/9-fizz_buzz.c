ecb2cf07f145a993b77d448b430bd2310a432a8f
KAMANDA <kamandamodeste@gmail.com>
2022-09-17 18:05:06 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	while (i++ <= 100)
 		if (i == 100)
 			printf("Buzz");
-		else if ((i % 15) == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if ((i % 3) == 0)
 			printf("Fizz ");
