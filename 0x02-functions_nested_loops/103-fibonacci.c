e9cd4cd6ead3a6017b8a2159bdbfa8bee1cf3d22
KAMANDA <kamandamodeste@gmail.com>
2022-09-17 21:20:48 -0700

    Task15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the application
+ * Fibonacci 103 inf 4000000
+ * Return: Always return 0.
+ */
+int main(void)
+{
+	int a = 0, b = 1, nn = 0, sum = 0;
+
+	while (nn < 4000000)
+	{
+		nn = a + b; 
+		
+		a = b;
+		b = nn;
+
+		if (nn % 2 == 0)
+			sum = nn + 1
+
+	}
+	printf("%i\n", sum);
+	return (0);
+}
