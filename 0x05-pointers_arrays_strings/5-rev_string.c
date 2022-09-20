21d14e69b1ad044f8f502c18b82be8c753f29408
KAMANDA <kamandamodeste@gmail.com>
2022-09-20 07:49:41 -0700

    Task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,21 @@
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	char c;
+	int i, j, k;
+
+	j = 0;
+	k = 0;
+
+	while (s[j] != '\0')
+		j++;
+
+	k = j - 1;
+	for (i = 0; i < j / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[k];
+		s[k] = c;
+		k -= 1;
+	}
 }
