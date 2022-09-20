742a0ee615ac6130363b805adc436a36d4d99519
KAMANDA <kamandamodeste@gmail.com>
2022-09-20 07:08:25 -0700

    Task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function that reverses a string
+ * @s: parameter to be used
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	strrev(s);
+}
