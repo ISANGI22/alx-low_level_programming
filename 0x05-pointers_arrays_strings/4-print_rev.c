30847baeb1a635425e8570692cdabc9349f0aac0
KAMANDA <kamandamodeste@gmail.com>
2022-09-20 06:03:03 -0700

    Task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print a strinf in reverse followed by a new line
+ * @s: parameter to be used 
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+	int i, n;
+	n = strlens(s);
+	for (i = n-1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
