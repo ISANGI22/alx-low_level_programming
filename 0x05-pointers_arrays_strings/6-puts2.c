e14267bf2c85451b87ac99528e3367ce20f381fc
KAMANDA <kamandamodeste@gmail.com>
2022-09-20 08:24:24 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - function that prints every other character of a string
+ *         starting with the first character followed by a new line
+ * @str: parameter to be used
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	j = strlen(str);
+	for (i = 0; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
