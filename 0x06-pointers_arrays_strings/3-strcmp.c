ffbf4bc9ec2c330b8561899437d4dbb338804ee4
KAMANDA <kamandamodeste@gmail.com>
2022-09-22 06:11:26 -0700

    Task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function that compares two strings
+ *
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: Return 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j = 1;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[2])
+		{
+			j = 0;
+			break;
+		}
+	}
+	return (j);
+}
