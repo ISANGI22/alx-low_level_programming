17ee6cc52f664ff9a96358ee1e711fb1d9922ff5
KAMANDA <kamandamodeste@gmail.com>
2022-09-22 20:02:44 -0700

    Task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *cap_string - function that capitalizes all words of a string
+ *
+ * @str: string to be used
+ *
+ * Return: a string
+ */
+char *cap_string(char *str)
+{
+	char eg[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i = 13;
+	int j = 0, k;
+
+	while (str[j])
+	{
+		k = 0;
+		while (k < i)
+		{
+			if ((j == 0 || str[j - 1] == eg[i]) && (str[j] >= 97 && str[j] <= 122)
+				str[j] = str[j] - 32;
+			k++;
+		}
+		j++;
+	}
+	return (str);
+}
