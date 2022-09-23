adc0143e23c0aa3ba1d2b2812ad818db8abac528
KAMANDA <kamandamodeste@gmail.com>
2022-09-22 19:03:01 -0700

    Task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (i = 0; j = n - 1; i < j; i++, j--)
+	for (i = 0; i < (n / 2); i++)
 	{
-		int k = a[i];
-		a[i] = a[j];
-		a[j] = k;
+		j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
 	}
 }
