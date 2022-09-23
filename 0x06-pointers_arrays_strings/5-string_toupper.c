2d6c07c1a169f4b20479003daad25175f90dcb8e
KAMANDA <kamandamodeste@gmail.com>
2022-09-22 19:22:14 -0700

    Task6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Function that changes all lowercase letter to a string to uppercase
+ *
+ * @i: caracter to be used
+ *
+ * Return: Always 0
+ */
+char *string_toupper(char *i)
+{
+	int j = 0;
+	while (i[j])
+	{
+		if (i[j] >= 97 && i[j] <= 122)
+			i[j] = i[j] - 32;
+		j++
+	}
+}
