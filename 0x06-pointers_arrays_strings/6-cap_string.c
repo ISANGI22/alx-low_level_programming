4af849acb282bac4a5c6edeabba10f4442973355
KAMANDA <kamandamodeste@gmail.com>
2022-09-23 07:27:03 -0700

    Task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,14 @@ char *cap_string(char *str)
 {
 	int j = 0, k;
 	char eg[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int lon = 13;
 
 	while (str[j])
 	{
 		k = 0;
-		while (k < 13)
+		while (k < lon)
 		{
-			if ((j == 0 || str[j - 1] == eg[j]) && (str[j] >= 'a' && str[j] <= 'z'))
+			if ((j == 0 || str[j - 1] == eg[k]) && (str[j] >= 'a' && str[j] <= 'z'))
 				str[j] = str[j] - 32;
 			k++;
 		}
