b821b5e272e6aad151eee036f062625851944861
KAMANDA <kamandamodeste@gmail.com>
2022-09-27 19:52:29 -0700

    Task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* *_memcpy - Function that copies memory area
+*
+* @dest: character destination
+* @src: character source
+* @n: interger to be used 
+*
+* Return: charater dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	char *d = dest;
+	char *s = src;
+
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
