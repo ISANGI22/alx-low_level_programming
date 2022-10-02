877c4cc19b30f0d5fb8df3a009b994bb4525b3bb
KAMANDA <kamandamodeste@gmail.com>
2022-10-02 03:35:44 -0700

    Task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #define UNUSED(x) (void)(x)
 
+/**
+ * main - Function that prints its name followed by a new line
+ *
+ * @argc: argument to count the parameters
+ * @argv: argument to hlod the parameters
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
 	UNUSED(argc);
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
