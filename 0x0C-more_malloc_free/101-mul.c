2034a2f82083558e331bc7559fe6f75b24f51e17
KAMANDA <kamandamodeste@gmail.com>
2022-10-12 01:48:34 -0700

    Task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
 				printf("Error\n");
 				exit(98);
@@ -36,4 +36,3 @@ int main(int argc, char *argv[])
 	printf("%lu\n", mul);
 	return (0);
 }
-
