4e6a75eeaa1142d54eacec2adb97919669a490a0
KAMANDA <kamandamodeste@gmail.com>
2022-10-15 23:56:57 -0700

    Task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Function that prints a struct dog
+ *
+ * @d: pointer to a struct or new dog
+ *
+ * Return: Always 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit (EXIT_SUCCESS);
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d-> = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,6 @@ typedef struct dog dog_s;
 /*prototype*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
