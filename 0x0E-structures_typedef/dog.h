5b4386ae12d57c47412abf6d5acbd7679f505aea
KAMANDA <kamandamodeste@gmail.com>
2022-10-15 23:16:08 -0700

    Task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,6 @@
  * @name: name given to a specific dog
  * @age: age of a specific dog
  * @owner: a person responsible of a dog
- * @new_dog: the new dog to be used in the function
- * Description: THis struct will be used to define a dog with some
- * basic information
  */
 struct dog
 {
