ad11b602476c1b60f1862bfc5caf170a88469b56
KAMANDA <kamandamodeste@gmail.com>
2022-10-28 22:34:18 -0700

    Task13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,83 @@
+#include "lists.h"
+
+size_t looped_listint_count(listint_t *head);
+
+/**
+ * looped_listint_count - Function that counts the number of the unique node
+ * in the looped listint_t liked list
+ *
+ * @head: a point to the head of the listint_t
+ *
+ * Return: if the list is not a looped 0 otherwise the number of the unique
+ * nodes in the list
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+			return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
+/**
+ * free_litint_safe - Function that frees a listint_t
+ *
+ * @h: parameter to be used in the function
+ *
+ * Return: the size of the freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	size_t nodes, index;
+
+	nodes = looped_listint_count(*h);
+	if (nodes == 0)
+	{
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+		*h = NULL;
+	}
+	h = NULL;
+	return (nodes);
+}
